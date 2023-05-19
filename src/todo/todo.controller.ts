52d52ad0de2c7f780e6c0aa40d1a4faa87db50c5
sutha.w <sutha.w@touchtechnologies.co.th>
2023-05-19 09:29:06 +0700

    fix nest 01

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { TodoService } from './todo.service';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private todoService: TodoService) {}
+
+  //   postTodo(@Body('title') title: string, @Body('subtitle') subtitle: string) {
+  //     console.log(`title:==>${title},subtitle=>${subtitle}`);
+  //   }
+
+  @Post()
+  postTodo(@Body('subtitle') subtitle: string) {
+    this.todoService.addTodo(subtitle);
+  }
+  @Get()
+  getTodos() {
+    return this.todoService.getTodos();
+  }
+}
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Todo } from './todo.entity';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  todoArray: Todo[];
+
+  addTodo(subtitle: string) {
+    console.log(`=>>>===>${subtitle}`);
+    const todo = new Todo();
+    todo.id = '1';
+    todo.subtitle = subtitle;
+
+    this.todoArray.push(todo);
+  }
+
+  getTodos() {
+    return this.todoArray;
+  }
+}
